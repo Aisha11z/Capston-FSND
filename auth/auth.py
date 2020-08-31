887522393036933845f3cf5a75f9d5413d5f1c0a
Aisha Abdullah <aishaalghamdi1997@gmail.com>
2020-09-01 00:49:40 +0300

    -

--- auth/auth.py
+++ auth/auth.py
@@ -5,9 +5,16 @@ from jose import jwt
 from urllib.request import urlopen
 
 
-AUTH0_DOMAIN = os.environ['AUTH0_DOMAIN']
-ALGORITHMS = os.environ['ALGORITHMS']
-API_AUDIENCE = os.environ['API_AUDIENCE']
+
+ENV = 'prod'
+if ENV == 'dev':
+    AUTH0_DOMAIN = 'csfs2020.us.auth0.com'
+    ALGORITHMS = ['RS256']
+    API_AUDIENCE = 'capstone'
+else:
+    AUTH0_DOMAIN = os.environ['AUTH0_DOMAIN']
+    ALGORITHMS = os.environ['ALGORITHMS']
+    API_AUDIENCE = os.environ['API_AUDIENCE']
 
 #AuthError Exception
 
