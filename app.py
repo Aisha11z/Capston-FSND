08d0e2add2e3e947df073fe0684a082bc4fb73b9
Aisha Abdullah <aishaalghamdi1997@gmail.com>
2020-08-31 13:36:34 +0300

    adding comment

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ from sqlalchemy.exc import SQLAlchemyError
 
 
 def create_app(test_config=None):
+    # variable set to 'prod' when production and set to 'dev' while
     ENV = 'prod'
     app = Flask(__name__)
     setup_db(app)
@@ -25,75 +26,86 @@ def create_app(test_config=None):
         ('Access-Control-Allow-Methods', 'GET,PATCH,POST,DELETE')
         return response
 
+    # the default endpoint ,will render html file instead of the front end
     @app.route('/')
     def index():
         return render_template('index.html')
 
-
+    # this endpoint to retrieve the movie list ,needs auth user and admin to see the output
     @app.route('/movies', methods=['GET'])
     @requires_auth('get:movies')
     def get_movies(payload):
 
-        movies = Movie.query.all()
-        if len(movies) == 0:
-            Movies_list = {}
-        else:
-            Movies_list = []
-            
-            for movie in movies:
-                
-                Movies_list.append({
-                    "id": movie.id,
-                    "title": movie.title,
-                    "rate": movie.rate,
-                    "directors":[director.id for director in Director.query.filter_by(movie_id=movie.id).all()]
-                   
-                })
-       
-        return jsonify({
-            'Movies_list': Movies_list,
-            'status': 200,
-            'success': True,
+        try:
+            # get all the movie from DB
+            movies = Movie.query.all()
 
-        })
+            if len(movies) == 0:
+                Movies_list = {}
+            else:
+                Movies_list = []
+
+                for movie in movies:
+
+                    Movies_list.append({
+                        "id": movie.id,
+                        "title": movie.title,
+                        "rate": movie.rate,
+                        "directors": [director.id for director in Director.query.filter_by(movie_id=movie.id).all()]
+
+                    })
+
+            return jsonify({
+                'Movies_list': Movies_list,
+                'status': 200,
+                'success': True,
+
+            })
+        except:
+            abort(422)
+
+    # this endpoint to retrieve the directors list ,needs auth user and admin to see the output
+
     @app.route('/directors', methods=['GET'])
     @requires_auth('get:directors')
     def get_directors(payload):
 
-       
-        directores = Director.query.all()
-        if len(directores) == 0:
-            directores_list = {}
-        else:
-            directores_list = []
-            for director in directores:
-                directores_list.append({
+        try:
+            # get all the directors from DB
+            directores = Director.query.all()
+            if len(directores) == 0:
+                directores_list = {}
+            else:
+                directores_list = []
+                for director in directores:
+                    directores_list.append({
                         "id": director.id,
                         "name": director.name,
-                        "movie_id":director.movie_id
+                        "movie_id": director.movie_id
                     })
-       
-        return jsonify({
-            'directores_list': directores_list,
-            'status': 200,
-            'success': True,
-        })
+            return jsonify({
+                'directores_list': directores_list,
+                'status': 200,
+                'success': True,
+            })
+        except:
+            abort(422)
 
-   
+    # this endpoint to add a movie to the DB , only admin can add
+
     @app.route('/movie', methods=['POST'])
     @requires_auth('post:movie')
     def add_movie(payload):
         body = request.get_json()
 
-       
         if 'movie_title' not in body:
             abort(422)
         if 'movie_rate' not in body:
-            abort (422)
+            abort(422)
         try:
             movie_title = body['movie_title']
             movie_rate = body['movie_rate']
-            movie = Movie(title=movie_title,rate=movie_rate)
+            movie = Movie(title=movie_title, rate=movie_rate)
             movie.insert()
             return jsonify({
                 'status': 200,
@@ -101,17 +113,18 @@ def create_app(test_config=None):
             })
         except:
             abort(422)
-    
+    # this endpoint to add a director to the DB , only admin can add
+
     @app.route('/director', methods=['POST'])
     @requires_auth('post:director')
     def add_director(payload):
         body = request.get_json()
         director_name = body.get('director_name', None)
-        movie_id=body.get('movie_id', None)
+        movie_id = body.get('movie_id', None)
         if director_name is None or movie_id is None:
             abort(422)
         try:
-            director = Director(name=director_name,movie_id=movie_id)
+            director = Director(name=director_name, movie_id=movie_id)
             director.insert()
             return jsonify({
                 'status': 200,
@@ -119,17 +132,17 @@ def create_app(test_config=None):
             })
         except:
             abort(422)
-    
 
+    # this endpoint to update a movie information , only admin can
 
     @app.route('/movie/<int:movie_id>', methods=['PATCH'])
     @requires_auth('patch:movie')
-    def update_movie( payload,movie_id):
+    def update_movie(payload, movie_id):
         movie = Movie.query.filter(Movie.id == movie_id).one_or_none()
-        
+
         if movie is None:
             abort(422)
-       
+
         body = request.get_json()
 
         try:
@@ -145,16 +158,17 @@ def create_app(test_config=None):
         except:
             abort(422)
 
+    # this endpoint to delete a movie frome the DB , only admin can
     @app.route('/movies/<int:movie_id>', methods=['DELETE'])
     @requires_auth('delete:movie')
-    def delete_movie( payload,movie_id):
+    def delete_movie(payload, movie_id):
         movie = Movie.query.filter(Movie.id == movie_id).first()
 
         if movie is None:
             abort(404)
 
         try:
-            directors=Director.query.filter_by(movie_id=movie.id).all()
+            directors = Director.query.filter_by(movie_id=movie.id).all()
             if len(directors) != 0:
                 for director in directors:
                     director.delete()
@@ -163,9 +177,8 @@ def create_app(test_config=None):
                 'status': 200,
                 'success': True,
             })
-        except SQLAlchemyError as e:
-            error= str(e.__dict__['orig'])
-            return error
+        except:
+            abort(422)
 
     '''
     Error Handling
@@ -211,6 +224,7 @@ def create_app(test_config=None):
 
     print(__name__, flush=True)
     if __name__ == '__main__':
+        # check if we are the in production or developing mood
         if ENV == 'dev':
             app.run(host='127.0.0.1', port=5000, debug=True)
         else:
